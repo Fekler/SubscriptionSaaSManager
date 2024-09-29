7daed71714314e106a6e194cda5e04455e8bc988
Felipe Queiroz <fekler.jobs@gmail.com>
2024-09-29 17:03:11 -0300

    Adição do postgresql

---
+++ SubscriptionSaaSManager.InfraData/Repositories/TenantRepository.cs
@@ -0,0 +1,16 @@
+﻿using SubscriptionSaaSManager.Domain.Entities;
+using SubscriptionSaaSManager.InfraData.Context;
+using SubscriptionSaaSManager.InfraData.Interfaces;
+using SubscriptionSaaSManager.InfraData.Repositories._bases;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SubscriptionSaaSManager.InfraData.Repositories
+{
+    public class TenantRepository(ApplicationDbContext context) : RepositoryBase<Tenant>(context), ITenantyRepository
+    {
+    }
+}
